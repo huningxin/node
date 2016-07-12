7fd1720e6f6b0bdaffe445cf8913ae9042401ad3
Ningxin Hu <ningxin.hu@intel.com>
2016-07-12 17:46:00 +0800

    Revert "Don't handle external strings"
    This reverts commit de3c42c3ae9125885466949cbe013ec8e2f3a8c5.

--- src/string_bytes.cc
+++ src/string_bytes.cc
@@ -295,7 +295,6 @@ bool StringBytes::GetExternalParts(Isolate* isolate,
     return true;
   }
 
-#if 0
   if (!val->IsString())
     return false;
 
@@ -315,7 +314,6 @@ bool StringBytes::GetExternalParts(Isolate* isolate,
     *len = ext->length() * sizeof(*ext->data());
     return true;
   }
-#endif
 
   return false;
 }
